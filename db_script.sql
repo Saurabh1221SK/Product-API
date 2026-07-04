071778472fd9cd9be86cfa45bce2f23c56bb84e6
Saurabh1221SK <saurabhkumar97408@gmail.com>
2026-07-05 02:12:51 +0530

    Add Product API CRUD Implementation

---
+++ db_script.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE product_db;
+USE product_db;
+CREATE TABLE products(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
